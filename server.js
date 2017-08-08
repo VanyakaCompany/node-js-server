4f99eddcb5b9b75d09eff57d02742847905a1e68
Vanyaka <VanyakaCompany@yandex.ru>
2017-08-08 17:08:06 +0300

    Шаблонизация (часть первая) v.1
    Подключен модуль handlebars.
    Добавлены 6 файлов с HTML кодом (в формате .hbs), 1 CSS файл и несколько картинок.
    Соответственно изменён файл сервера.

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
 // Подключаемые модули
 var express = require('express');
 var url = require("url");
+var hbs = require("hbs");
 
 
-var app = express();  // Создаем объект приложения
+var app = express();  // Привязываем express к нашему "приложению"
+app.use(express.static(__dirname + '/public')); // Теперь мы сможем обращаться из браузера к любым файлам в этой папке
 
 
-// Блок переменных
-var pathname = '';  // Переменная для хранения текущего url
-var result;         // Переменная для хранения результата вызываемых ф-й
-
-// База данных
-var bd = [
-  {url: '/', content: '<h3>Главная страница</h3><a href="/post-1.html">Статья №1</a><br><a href="/post-2.html">Статья №2</a>'},
-  {url: '/about', content: '<h3>О нас</h3>'},
-  {url: '/contacts', content: '<h3>Наши контакты</h3>'},
-  {url: '/post-1.html', content: '<h3>Статья №1</h3>'},
-  {url: '/post-2.html', content: '<h3>Статья №2</h3>'}
-];
+// Для шаблонов страниц
+hbs.registerPartials(__dirname + "/views/templates", function() {
+  app.set("view engine", "hbs");
 
 
-// Обработчик запросов
-app.use(function(req, res) {
-  // Определяем текущий url
-  pathname = url.parse(req.url).pathname;
+  // Блок переменных
+  var pathname = '';  // Переменная для хранения текущего url
+  var result;         // Переменная для хранения результата вызываемых ф-й
 
-  // Проверяем url
-  result = find(bd , pathname);
-  if(result != -1) {
-    res.send(bd[result].content);                     // Если страница найдена, то отправить её на клиент
-  } else {
-    res.status(404).send('Страница не найдена...');   // Иначе, отправить ошибку 404
-  }
+  // База данных
+  var bd = [
+    {url: '/', file: 'index.hbs'},
+    {url: '/about', file: 'about.hbs'},
+    {url: '/contacts', file: 'contacts.hbs'},
+    {url: '/post-1.html', file: 'post_1.hbs'},
+    {url: '/post-2.html', file: 'post_2.hbs'}
+  ];
+
+
+  // Обработчик запросов
+  app.use(function(req, res, next) {
+    // Определяем текущий url
+    pathname = url.parse(req.url).pathname;
+
+    // Проверяем url
+    result = find(bd , pathname);
+    if(result != -1) {
+      res.render(bd[result].file);         // Если страница найдена, то отправить соответствующий файл на клиент
+    } else {
+      res.status(404).render('404.hbs');   // Иначе, отправить ошибку 404
+    }
+  });
+
+
+  app.listen(3000);   // Установка порта
 });
-
-app.listen(3000);   // Установка порта
 
 
 // Функция поиска в массиве
